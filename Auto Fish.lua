5d1fa6d0abb9e4d31eb54a25100ca5bae13123a0
vroom8742 <74122086+vroom8742@users.noreply.github.com>
2021-01-01 23:35:05 -0600

    Create Auto Fish.lua

---
+++ Auto Fish.lua
@@ -0,0 +1,26 @@
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local Character = player.character
+
+local Rod
+for i,v in pairs(Character:GetChildren()) do
+    if v:IsA("Tool") and string.match(v.Name, "Rod") then
+        Rod = v
+    end
+end
+
+local FishState = game:GetService("ReplicatedStorage").RS.Remotes.Misc.FishState
+local ToolAction = game:GetService("ReplicatedStorage").RS.Remotes.Misc.ToolAction
+
+getgenv().AUTOFISH = true
+pcall(function()
+    while getgenv().AUTOFISH do
+        if Character:FindFirstChild("FishBiteGoal") ~= nil then
+            FishState:FireServer("Reel")
+            ToolAction:FireServer(Rod)
+        elseif Character:FindFirstChild("FishClock") == nil then
+            ToolAction:FireServer(Rod)
+            wait(5)
+        end
+    end
+end)
+
